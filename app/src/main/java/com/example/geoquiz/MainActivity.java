47920b76ebd5e3346d924d8a69f5fab39245e4e2
Vladimir Gudz <gudz.vladimir@gmail.com>
2019-10-07 16:18:53 +0300

    New project

---
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.geoquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+        private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_1,true),
+            new Question(R.string.question_2,false),
+            new Question(R.string.question_3,true),
+            new Question(R.string.question_4, true)
+        };
+        private int mCurrentIndex=0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mQuestionTextView=(TextView) findViewById(R.id.question_text_view);
+        int question =
+
+            mTrueButton = (Button) findViewById(R.id.true_button);
+                mTrueButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this,R.string.incorrect_toast,Toast.LENGTH_LONG).show();
+                    }
+                });
+            mFalseButton = (Button) findViewById(R.id.false_button);
+                mFalseButton.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v){
+                        Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/geoquiz/Question.java
@@ -0,0 +1,29 @@
+package com.example.geoquiz;
+    public class Question {
+
+        private int mTextResId;
+        private boolean mAnswerTrue;
+
+        public int getTextResId() {
+            return mTextResId;
+        }
+
+        public void setTextResId(int textResId) {
+            mTextResId = textResId;
+        }
+
+        public boolean isAnswerTrue() {
+            return mAnswerTrue;
+        }
+
+        public void setAnswerTrue(boolean answerTrue) {
+            mAnswerTrue = answerTrue;
+        }
+
+        public Question(int TextResId, boolean AnswerTrue){
+        mTextResId=TextResId;
+        mAnswerTrue=AnswerTrue;
+
+
+        }
+    }
