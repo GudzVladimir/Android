706c231365726d9ebc0eba9dcd65d3781705f853
Vladimir Gudz <gudz.vladimir@gmail.com>
2019-10-12 16:56:01 +0300

    Add button NEXT

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -29,7 +29,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mQuestionTextView=(TextView) findViewById(R.id.question_text_view);
-        int question =
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+            mQuestionTextView.setText(question);
+
 
             mTrueButton = (Button) findViewById(R.id.true_button);
                 mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -38,6 +40,16 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(MainActivity.this,R.string.incorrect_toast,Toast.LENGTH_LONG).show();
                     }
                 });
+        mNextButton=(Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex+1)% mQuestionBank.length;
+                int question = mQuestionBank[mCurrentIndex].getTextResId();
+                mQuestionTextView.setText(question);
+            }
+        });
+
             mFalseButton = (Button) findViewById(R.id.false_button);
                 mFalseButton.setOnClickListener(new View.OnClickListener(){
                     @Override
